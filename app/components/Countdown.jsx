da4b773dd1923faf321ef83fb41b859f78ef5a71
David Alberto Hans Castillo <dhansc@bch.bancodechile.cl>
2017-11-06 12:30:41 -0300

    ADD: countdown logic

--- app/components/Countdown.jsx
+++ app/components/Countdown.jsx
@@ -5,12 +5,38 @@ let CountdownForm = require('CountdownForm');
 let Countdown = React.createClass({
 	getInitialState: function(argument) {
 		return {
-			count: 0
+			count: 0,
+			countDownStatus: 'stopped'
 		}
+	},
+	componentDidUpdate: function(prevProps,prevState) {
+		if(this.state.countDownStatus !== prevState.countDownStatus){
+			switch(this.state.countDownStatus){
+				case 'started':
+					this.startTimer();
+					break;
+				case 'stopped':
+					clearInterval(this.timer);
+			}
+		}
+	},
+	startTimer: function(){
+		this.timer = setInterval(() => {
+			let newCount = this.state.count - 1;
+			let tempState = {};
+			if(newCount < 0){
+				tempState.countDownStatus = 'stopped';
+			} else {
+				tempState.count = newCount;
+			}
+			this.setState(tempState);
+			console.log('Count',newCount,this.state.countDownStatus);
+		},1000);
 	},
 	handleSetCountdown: function(totalSeconds){
 		this.setState({
-			count: totalSeconds
+			count: totalSeconds,
+			countDownStatus: 'started'
 		});
 	},
 	render: function(){
